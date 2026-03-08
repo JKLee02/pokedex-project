4bde367839b75773155b10505cb834b3e8fbdf95
JKLee02 <talentlee2002@gmail.com>
2026-03-08 15:43:28 +0800

    Another minor changes to the image optimization

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   /* config options here */
   images: {
     imageSizes: [32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     remotePatterns: [
       {
         protocol: "https",
