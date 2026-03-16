7870014dcb6464d44fe42d05fd718cfded85b46e
JKLee02 <talentlee2002@gmail.com>
2026-03-16 19:18:33 +0800

    Updated the pokemon grid without side images and minor improvements

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,7 +2,6 @@
 
 import Carousel from "./components/Carousel";
 import StaticBanners from "./components/StaticBanners";
-import SideImage from "./components/SideImage";
 import PokemonGrid from "./components/PokemonGrid";
 import { usePokemon } from "./components/PokemonContext";
 
@@ -22,39 +21,22 @@ export default function PokedexPage() {
       {/* Top Section - Carousel and Static Banners */}
       <div className="top-section">
         <div className="top-grid">
-          {/* Carousel and static banners components */}
           <Carousel />
           <StaticBanners />
         </div>
       </div>
 
-      {/* Middle Section with Persistent Layout */}
+      {/* Middle Section - Pokemon Grid */}
       <div className="middle-section">
-        <div className="middle-grid">
-          {/* Left Static Image */}
-
-          <SideImage
-            src="/images/pokecards2.jpg"
-            alt="Left side of static image"
-          />
-
-          {/* Center - Scrollable Pokemon List */}
-          <PokemonGrid
-            pokemon={filteredPokemon}
-            handleSearch={handleSearch}
-            searchQuery={searchQuery}
-            setSearchQuery={setSearchQuery}
-            loadMore={loadMore}
-            loading={loading}
-            hasMore={hasMore}
-          />
-
-          {/* Right Static Image */}
-          <SideImage
-            src="/images/pikachu.png"
-            alt="right side of static image"
-          />
-        </div>
+        <PokemonGrid
+          pokemon={filteredPokemon}
+          handleSearch={handleSearch}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          loadMore={loadMore}
+          loading={loading}
+          hasMore={hasMore}
+        />
       </div>
     </div>
   );
